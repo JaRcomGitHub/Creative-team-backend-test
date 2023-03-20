e263d4c99b15fceac22565072dfe14fbd85266ba
JaRcomGitHub <jarcom@ukr.net>
2023-03-20 22:14:03 +0200

    Update getSelected.js

--- src/controllers/notices/getSelected.js
+++ src/controllers/notices/getSelected.js
@@ -1,5 +1,21 @@
+const { User } = require("../../schemas/user");
+const { Notice } = require("../../schemas/notice");
+
 async function getSelected(req, res) {
-  return res.status(504).json({ message: "getSelected not implemented" });
+  try {
+    const { id } = req.user;
+    const user = await User.findById(id);
+    // console.log("user.selected", user.selected);
+
+    // const result = await Notice.find({
+    //   _id: { $elemMatch: { address: "user@gmail.com" } },
+    // });
+    // console.log("result", result);
+
+    return res.status(200).json({ selected: user.selected });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 }
 
 module.exports = getSelected;
