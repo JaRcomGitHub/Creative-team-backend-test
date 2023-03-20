c50615e83f87e03e1368942a5f0ddc2c4ddedca5
JaRcomGitHub <jarcom@ukr.net>
2023-03-20 21:41:07 +0200

    Update noticesRouter.js

--- src/routes/noticesRouter.js
+++ src/routes/noticesRouter.js
@@ -5,17 +5,21 @@ const { authMiddleware } = require("../middlewares/auth");
 const router = express.Router();
 
 router.get("/user", authMiddleware, noticeController.getUserNotices);
+router.get("/selected", authMiddleware, noticeController.getSelected);
 router.get("/title", noticeController.searchByTitle);
 router.get("/category", noticeController.getNoticesByCategory);
 router.get("/:noticeId", noticeController.getNotice);
-router.post("/selected", authMiddleware, noticeController.addSelected);
-router.get("/selected", authMiddleware, noticeController.getSelected);
+router.post("/user", authMiddleware, noticeController.addUserNotice);
+router.post(
+  "/selected/:noticeId",
+  authMiddleware,
+  noticeController.addSelected
+);
 router.delete(
   "/selected/:noticeId",
   authMiddleware,
   noticeController.deleteSelected
 );
-router.post("/user", authMiddleware, noticeController.addUserNotice);
 router.delete(
   "/user/:noticeId",
   authMiddleware,
