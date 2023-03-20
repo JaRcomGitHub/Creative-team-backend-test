b4bddff1d625cafa14e38ac3c748712f56be820c
JaRcomGitHub <jarcom@ukr.net>
2023-03-20 18:12:26 +0200

    Update signup.js

--- src/controllers/users/signup.js
+++ src/controllers/users/signup.js
@@ -1,9 +1,15 @@
 const { User } = require("../../schemas/user");
-const { userJoiValidation } = require("../../services");
+const { authSchema } = require("../../schemas/joiValidation");
+const jwt = require("jsonwebtoken");
+const { SECRET } = process.env;
 
 async function signup(req, res) {
   // joi validation
-  userJoiValidation(req.body);
+  const { error } = authSchema.validate(req.body);
+  if (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
 
   const { email, password } = req.body;
   console.log(email);
@@ -23,10 +29,23 @@ async function signup(req, res) {
   // if not - hash password and create user
   const newUser = await User.create({ ...req.body });
   newUser.setPassword(password);
-  await newUser.save();
+
+  // generate token
+  const payload = { id: newUser.id };
+  const token = jwt.sign(payload, SECRET, { expiresIn: "12h" });
+
+  // write token to user
+  newUser.accessToken = token;
+
+  const updated = await newUser.save();
+  if (!updated) {
+    res.status(400);
+    throw new Error("Unable create user");
+  }
 
   return res.status(201).json({
-    user: { email: newUser.email },
+    token: newUser.accessToken,
+    user: newUser.email,
   });
 }
 
