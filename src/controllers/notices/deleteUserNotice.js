ec0c0859d0189f012d281d237b96d034b19641ff
JaRcomGitHub <jarcom@ukr.net>
2023-03-21 22:43:00 +0200

    Update deleteUserNotice.js

--- src/controllers/notices/deleteUserNotice.js
+++ src/controllers/notices/deleteUserNotice.js
@@ -1,5 +1,16 @@
+const { Notice } = require("../../schemas/notice");
+
 async function deleteUserNotice(req, res) {
-  return res.status(504).json({ message: "deleteUserNotice not implemented" });
+  const { noticeId } = req.params;
+
+  const removeResult = await Notice.findByIdAndRemove(noticeId);
+
+  if (!removeResult) {
+    res.status(404);
+    throw new Error("Not found");
+  }
+
+  return res.status(200).json({ message: "notice was removed" });
 }
 
 module.exports = deleteUserNotice;
