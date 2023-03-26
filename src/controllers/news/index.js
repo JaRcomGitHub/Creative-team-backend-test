f0056025c413779d24ba91c9240a59291d9f8842
JaRcomGitHub <jarcom@ukr.net>
2023-03-26 11:50:06 +0300

    Update index.js

--- src/controllers/news/index.js
+++ src/controllers/news/index.js
@@ -7,11 +7,22 @@ async function news(req, res) {
     const searchResult = await New.find({
       title: { $regex: query, $options: "i" },
     });
-    return res.status(200).json(searchResult);
+    return res
+      .status(200)
+      .json(
+        Object.values(searchResult).sort(
+          (a, b) => new Date(b.date) - new Date(a.date)
+        )
+      );
   }
 
   const result = await New.find({});
-  return res.status(200).json(result);
+
+  return res
+    .status(200)
+    .json(
+      Object.values(result).sort((a, b) => new Date(b.date) - new Date(a.date))
+    );
 }
 
 module.exports = {
